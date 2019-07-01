d056ab9086522399af2df49c00bba2ad475cecb2
wangjiajia <wangjiajia_cumt@163.com>
2019-07-01 17:12:08 +0800

    * Fix a bug, first define every variable before Subscriber * Add a image copy for detection * Add a simple classfied function based on light color for real test bag,   because error classfied rate of the real image is too high. But the traffic light detection is ok

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -11,6 +11,7 @@ class TLClassifier(object):
     def __init__(self,is_site):
         # #TODO load classifier
         # Model preparation
+        self.is_site = is_site
         PATH_TO_FROZEN_GRAPH = 'light_classification/model/frozen_inference_graph_sim.pb'
         if is_site:
             PATH_TO_FROZEN_GRAPH = 'light_classification/model/frozen_inference_graph_site.pb'
@@ -32,7 +33,7 @@ class TLClassifier(object):
         
         self.image_result_show = True
 
-        self.light_thresh = 0.9
+        self.light_thresh = 0.4
         # create tensorflow session
         config = tf.ConfigProto()
         config.gpu_options.allow_growth = True
@@ -50,7 +51,7 @@ class TLClassifier(object):
                 self.tensor_dict[key] = self.detection_graph.get_tensor_by_name(
                 tensor_name)
         self.image_tensor = self.detection_graph.get_tensor_by_name('image_tensor:0')
-
+        self.current_light = TrafficLight.GREEN
     # def load_image_into_numpy_array(self, image):
     #     (im_width, im_height) = image.size
     #     return np.array(image.getdata()).reshape((im_height, im_width, 3)).astype(np.uint8)
@@ -70,6 +71,43 @@ class TLClassifier(object):
 
         return output_dict
 
+    def light_color(self,lightImg):
+        if self.image_result_show:
+            cv.namedWindow('light_image', cv.WINDOW_NORMAL)
+            cv.imshow('light_image', lightImg)
+            cv.waitKey(1)
+
+        lightImg = cv.cvtColor(lightImg, cv.COLOR_BGR2HSV)    # translate bgr to HSV
+        
+        height,width,depth = lightImg.shape 
+    
+        height_start = int (height/8)      # split 3 part in height 
+        height_1st = int(height/3)        
+        height_2nd = int(height*2/3)      
+        height_end = int(height*7/8)
+
+        lightImg_red = lightImg[height_start:height_1st,:,2]     # red part image
+        lightImg_yellow = lightImg[height_1st:height_2nd,:,2]    # yellow part
+        lightImg_green = lightImg[height_2nd:height_end,:,2]     # green part
+
+        # Calculating the average of pixels each color part image
+        red_height,red_width = lightImg_red.shape
+        red_pixel_sum = np.sum(lightImg_red)/(red_height*red_width)
+
+        yellow_height,yellow_width = lightImg_yellow.shape
+        yellow_pixel_sum = np.sum(lightImg_yellow)/(yellow_height*yellow_width)
+
+        green_height,green_width = lightImg_green.shape
+        green_pixel_sum = np.sum(lightImg_green)/(green_height*green_width)
+
+        print ("red is%f, yellow is %f, green is %f"%(red_pixel_sum,yellow_pixel_sum,green_pixel_sum))
+        # red part is biggest indicate red light
+        if red_pixel_sum > yellow_pixel_sum and red_pixel_sum > green_pixel_sum:
+            current_light = TrafficLight.RED
+        else:
+            current_light = TrafficLight.GREEN
+        return current_light
+
     def get_classification(self, image):
         """Determines the color of the traffic light in the image
 
@@ -83,7 +121,8 @@ class TLClassifier(object):
         #TODO implement light color prediction
         # image_np = self.load_image_into_numpy_array(image)
         # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
-        image_np_expanded = np.expand_dims(image, axis=0)
+        image_cp = image.copy()
+        image_np_expanded = np.expand_dims(image_cp, axis=0)
 
         # Actual detection.
         output_dict = self.run_inference_for_single_image(image_np_expanded, self.detection_graph)
@@ -92,7 +131,7 @@ class TLClassifier(object):
         # print(output_dict['detection_scores'])
         # print(output_dict['detection_boxes'])
 
-        # Show classified result image
+        # Show classified result image for test
         if self.image_result_show:
             img_width,img_height,depth = image.shape
             font = cv.FONT_HERSHEY_COMPLEX
@@ -103,21 +142,42 @@ class TLClassifier(object):
                 botom_x = output_dict['detection_boxes'][i][3] * img_height
                 score = output_dict['detection_scores'][i]
                 class_name = self.category_index[output_dict['detection_classes'][i]]['name']
-                cv.rectangle(image, (int(top_x), int(top_y)), (int(botom_x), int(botom_y)), (0, 0, 255), thickness=2)
-                cv.putText(image, class_name + ' ' + str(score), (int(top_x), int(top_y)), font, 0.8,(255, 255, 255), 1)
+                if score>self.light_thresh:
+                    cv.rectangle(image_cp, (int(top_x), int(top_y)), (int(botom_x), int(botom_y)), (0, 0, 255), thickness=2)
+                    cv.putText(image_cp, str(class_name) + ' ' + str(score), (int(top_x), int(top_y)), font, 0.8,(255, 255, 255), 1)
             cv.namedWindow('result', cv.WINDOW_NORMAL)
-            cv.imshow('result', image)
+            cv.imshow('result', image_cp)
             cv.waitKey(1)
         
-        # Get red light max score
+        # init red light score
         red_score_max = 0
+        img_width,img_height,depth = image.shape
         for i in range(output_dict['num_detections']):
-            if output_dict['detection_classes'][i] == 2:
-                if output_dict['detection_scores'][i] > red_score_max:
+            if self.is_site or output_dict['detection_classes'][i]==2:        #if sim, just get red light max score 
+                if output_dict['detection_scores'][i] > red_score_max:        #if site, need get all traffic light max score
                     red_score_max = output_dict['detection_scores'][i]
-        if red_score_max > self.light_thresh:
-            current_light = TrafficLight.RED
+                    top_y = output_dict['detection_boxes'][i][0] * img_width
+                    top_x = output_dict['detection_boxes'][i][1] * img_height
+                    botom_y = output_dict['detection_boxes'][i][2] * img_width
+                    botom_x = output_dict['detection_boxes'][i][3] * img_height
+        if self.is_site:       # if site mode, need classified the light color based image color
+            if red_score_max > self.light_thresh:
+                lightImg = image[int(top_y):int(botom_y),int(top_x):int(botom_x)]   # crop traffic image
+                self.current_light = self.light_color(lightImg)        # get traffic light color
+            else:
+                self.current_light = TrafficLight.GREEN
         else:
-            current_light = TrafficLight.GREEN
-        print(current_light)    
-        return current_light
+            if red_score_max > self.light_thresh:
+                self.current_light = TrafficLight.RED
+            else:
+                self.current_light = TrafficLight.GREEN 
+
+        if self.current_light == TrafficLight.RED:
+            print('currend light is RED')
+        else:
+            print('currend light is GREEN')
+        return self.current_light
+            
+
+
+
